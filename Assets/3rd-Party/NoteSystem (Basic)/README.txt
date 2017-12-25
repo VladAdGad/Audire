a2b2dda0366648c1d511f6ab806b83e7afd3edc0
vutkin <vladyslav.utkin@icompass.pl>
2017-12-25 19:50:29 +0100

    Adding rest files to git.

---
+++ Assets/3rd-Party/NoteSystem (Basic)/README.txt
@@ -0,0 +1,14 @@
+To Use This System:
+
+	First: 	Drag a note prefab into your world.
+	Second: You can set the text field of the "Note" component of the new note game object in your world to anything you like
+	Next:	Now Drag The "PickupNote" script onto your player
+	Finally: If you look on the "PickupNote" script on your player you will see an option for Skin, just drag "NoteStyle" skin into this option and that's it!
+
+Now all you have to do is approach the cube (Which is a placeholder, you should replace the cube mesh with your own 3d model of a peice of paper) and press 'E' to open the note.
+When you want to close the note press 'E' again.
+
+You can modify the style of the note by editing the provided skin, and by changing the code in the OnGUI function of the "PickupNote" script
+
+
+Best Of Luck, CharlesD.
---
+++ Assets/3rd-Party/NoteSystem (Basic)/Scripts/PickupNote.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System.Collections;
+
+public class PickupNote : MonoBehaviour
+{
+    public bool hasCollided = false;
+
+    //Maximum Distance you Can Pick Up A Book
+    public float maxDistance = 100f;
+
+    //Your Custom GUI Skin with the Margins, Padding, Align, And Texture all up to you :)
+    public GUISkin skin;
+
+    //Are we currently reading a note?
+    private bool readingNote = false;
+
+    //The text of the note we last read
+    private string noteText;
+
+    private Camera m_camera;
+
+    public GameObject Sound;
+
+    void Start()
+    {
+        //Start the input check loop
+        m_camera = Camera.main;
+        StartCoroutine(CheckForInput());
+    }
+
+    private IEnumerator CheckForInput()
+    {
+        // Keep Updating
+        while (true)
+        {
+            //If the 'F' was pressed and not reading a note check for a note, else stop reading
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (readingNote)
+                {
+                    m_camera.enabled = true;
+                    readingNote = false;
+                }
+                else
+                {
+                    CheckForNote();
+                }
+            }
+
+            //Wait One Frame Before Continuing Loop
+            yield return null;
+        }
+    }
+
+    private void CheckForNote()
+    {
+        //A ray from the center of the screen
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2f, Screen.height / 2f));
+        RaycastHit hit;
+
+        //Did we hit something?
+        //Was the object we hit a note?
+        if (Physics.Raycast(ray, out hit, maxDistance) && hit.collider.GetComponent<Note>())
+        {
+            //Get text of note and set reading to true
+            noteText = hit.transform.GetComponent<Note>().Text;
+            //Destroy (data.transform.gameObject);
+            readingNote = true;
+            m_camera.enabled = false;
+            Sound.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (skin)
+        {
+            GUI.skin = skin;
+        }
+
+        //Are we reading a note? If so draw it.
+        if (readingNote)
+        {
+            //Draw the note on screen, Set And Change the GUI Style To Make the Text Appear The Way you Like (Even on an image background like paper)
+            GUI.Box(
+                new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 24, Screen.width / 3,
+                    Screen.height - Screen.height / 18), noteText);
+        }
+    }
+}
