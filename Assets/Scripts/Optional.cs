dd8bd62ad832b6a825c5efd6aaa519027601421c
vutkin <vladyslav.utkin@icompass.pl>
2017-12-26 01:36:53 +0100

    Fixed Optional.

--- Assets/Scripts/Optional.cs
+++ Assets/Scripts/Optional.cs
@@ -1,39 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace DefaultNamespace
+public class Optional<T>
 {
-    public class Optional<T>
+    private readonly T _value;
+
+    private Optional(T value)
     {
-        private readonly T _value;
-
-        private Optional(T value)
-        {
-            _value = value;
-        }
-
-        private Optional()
-        {
-            _value = default(T);
-        }
-
-        public static Optional<T> Of(T value) => new Optional<T>(value);
-        public static Optional<T> Empty() => new Optional<T>();
-
-        public Optional<V> Map<V>(Func<T, V> function) => IsPresent ? new Optional<V>() : new Optional<V>(function.Invoke(_value));
-        public Optional<T> Filter(Predicate<T> predicate) => predicate.Invoke(_value) ? this : Empty();
-        public void IfPresent(Action<T> action) => action.Invoke(_value);
-        public IEnumerable<V> FlatMap<V>(Func<T, IEnumerable<V>> function) => _value == null ? new List<V>() : function.Invoke(_value);  
-        public T OrElse(T secondary) => IsPresent ? secondary : _value;
-        public Optional<V> Cast<V>() where V : class => new Optional<V>(_value as V);
-        
-        public Optional<T> Do(Action<T> action) 
-        {
-            IfPresent(action);
-            
-            return this;
-        }
-
-        private bool IsPresent => _value == null;
+        _value = value;
     }
+
+    private Optional()
+    {
+        _value = default(T);
+    }
+
+    public static Optional<T> Of(T value) => new Optional<T>(value);
+    public static Optional<T> Empty() => new Optional<T>();
+
+    public Optional<V> Map<V>(Func<T, V> function) => IsPresent ? new Optional<V>(function.Invoke(_value)) : new Optional<V>();
+    public Optional<T> Filter(Predicate<T> predicate) => IsPresent && predicate.Invoke(_value) ? this : Empty();
+    public IEnumerable<V> FlatMap<V>(Func<T, IEnumerable<V>> function) => IsPresent ? function.Invoke(_value) : new List<V>();
+
+    public T OrElse(T secondary) => IsPresent ? _value : secondary;
+
+    public Optional<V> Cast<V>() where V : class => new Optional<V>(_value as V);
+
+    public Optional<T> Do(Action<T> action) 
+    {
+        IfPresent(action);
+            
+        return this;
+    }
+
+    public void IfPresent(Action<T> action)
+    {
+        if (IsPresent)
+        {
+            action.Invoke(_value);
+        }
+    }
+
+    private bool IsPresent => _value != null;
 }
