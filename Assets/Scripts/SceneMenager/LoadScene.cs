13a99bd98c10b23770352570b870ad0eda5fc10b
vutkin <vladyslav.utkin@icompass.pl>
2017-12-30 21:11:43 +0100

    17 created loads into the next scene

---
+++ Assets/Scripts/SceneMenager/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using Player;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneMenager
+{
+    public class LoadScene : MonoBehaviour
+    {
+        public static IEnumerator ChangeLevel()
+        {
+            float fadeTime = GameObject.Find("FPSController").GetComponent<Fading>().BeginFade(1);
+            yield return new WaitForSeconds(fadeTime);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+}
