c58fb043b09213db90459807e2e51367ea454977
vutkin <vladyslav.utkin@icompass.pl>
2017-12-31 22:25:39 +0100

    13 Implemented TextBehaviour

---
+++ Assets/Scripts/SceneMenager/TextColorBehaviuor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace SceneMenager
+{
+    public class TextColorBehaviuor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private Text _text;
+        [SerializeField] private Color _enterColor = Color.red;
+        [SerializeField] private Color _exitColor = Color.white;
+        
+        private void Start()
+        {
+            _text = GetComponent<Text>();
+            _text.color = _exitColor;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData) => _text.color = _enterColor;
+
+        public void OnPointerExit(PointerEventData eventData) => _text.color = _exitColor;
+
+        private void OnValidate()
+        {
+            Assert.IsNotNull(_text);
+        }
+    }
+}
