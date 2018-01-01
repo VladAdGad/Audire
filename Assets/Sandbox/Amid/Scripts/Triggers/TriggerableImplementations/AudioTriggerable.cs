aecbde5aded7ce5413fd23a45d247a58fa249001
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-01 04:46:37 +0100

    added AudioTriggerable

---
+++ Assets/Sandbox/Amid/Scripts/Triggers/TriggerableImplementations/AudioTriggerable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sandbox.Amid.Scripts;
+using UnityEngine;
+
+public class AudioTriggerable : ATriggerable
+{
+    public bool singleTrigger = true;
+    private bool _wasActivated = false;
+
+    private AudioSource _audioSource;
+
+    private void Start() => _audioSource = GetComponent<AudioSource>();
+
+    public override void TriggerEnter(Collider collider)
+    {
+        if (CanPlaySound)
+        {
+            _wasActivated = true;
+            _audioSource.Play();
+        }
+    }
+    
+    private bool CanPlaySound => singleTrigger || _wasActivated;
+
+    public override void TriggerExit(Collider collider)
+    {
+    }
+
+}
