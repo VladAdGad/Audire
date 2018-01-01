72c126e806c429eefc9f5493ba03b6f41e1d36c6
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-01 04:04:45 +0100

    added generic triggerable for trigger enter management

---
+++ Assets/Sandbox/Amid/Scripts/GenericTrigger.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sandbox.Amid.Scripts;
+using UnityEngine;
+
+public class GenericTrigger : MonoBehaviour
+{
+	[SerializeField] private List<ATriggerable> _triggerables; 
+	
+	void OnTriggerEnter(Collider other) => _triggerables.ForEach(it => it.TriggerEnter(other));
+	void OnTriggerExit(Collider other) => _triggerables.ForEach(it => it.TriggerExit(other));
+}
