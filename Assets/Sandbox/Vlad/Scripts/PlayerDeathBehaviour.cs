b2406818b557945d8c194b105801a894058fbad5
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-09 01:18:19 +0100

    19 added lvl1 to Vlad' sandox for working on death_script

---
+++ Assets/Sandbox/Vlad/Scripts/PlayerDeathBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using SceneMenager;
+using UnityEngine;
+
+namespace Sandbox.Vlad.Scripts
+{
+    public class PlayerDeathBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                StartCoroutine(LoadScene.ReloadScene());
+            }
+        }
+    }
+}
