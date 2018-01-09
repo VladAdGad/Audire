199847b55df2816a6f7c3670945bfa75cea2c5b0
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-09 01:19:15 +0100

    19 implemented reloadScene when player die

--- Assets/Scripts/SceneMenager/LoadScene.cs
+++ Assets/Scripts/SceneMenager/LoadScene.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using InteractableObjects;
 using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,12 +9,23 @@ namespace SceneMenager
     public class LoadScene : MonoBehaviour
     {
         private const int NextScene = 1;
+        private const int WaitBeforeLoad = 5;
 
         public static IEnumerator ChangeLevel()
         {
+            PlayerBehaviour.SetFirstControllerInteract(false);
             float fadeTime = GameObject.Find("FPSController").GetComponent<Fading>().BeginFade(1);
             yield return new WaitForSeconds(fadeTime);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + NextScene);
+        }
+
+        public static IEnumerator ReloadScene()
+        {
+            PlayerBehaviour.SetFirstControllerInteract(false);
+            float fadeTime = GameObject.Find("FPSController").GetComponent<Fading>().BeginFade(1);
+            yield return new WaitForSeconds(fadeTime);
+            yield return new WaitForSeconds(WaitBeforeLoad);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
