339a3d1cc378ef85f2bbfddffdb95c6b48f9b965
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-14 08:02:28 +0100

    19 refactored code

--- Assets/Scripts/SceneMenager/LoadScene.cs
+++ Assets/Scripts/SceneMenager/LoadScene.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using InteractableObjects;
 using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,7 +8,6 @@ namespace SceneMenager
     public class LoadScene : MonoBehaviour
     {
         private const int NextScene = 1;
-        private const int WaitBeforeLoad = 5;
 
         public static IEnumerator ChangeLevel()
         {
@@ -19,12 +17,8 @@ namespace SceneMenager
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + NextScene);
         }
 
-        public static IEnumerator ReloadScene()
+        public static void ReloadScene()
         {
-            PlayerBehaviour.SetFirstControllerInteract(false);
-            float fadeTime = GameObject.Find("FPSController").GetComponent<Fading>().BeginFade(1);
-            yield return new WaitForSeconds(fadeTime);
-            yield return new WaitForSeconds(WaitBeforeLoad);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
