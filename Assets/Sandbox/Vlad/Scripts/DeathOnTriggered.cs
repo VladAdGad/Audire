7eb1312343525960f4a561403ed9c8a587702d6e
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-14 22:54:59 +0100

    19 implemented sound in trigger

--- Assets/Sandbox/Vlad/Scripts/DeathOnTriggered.cs
+++ Assets/Sandbox/Vlad/Scripts/DeathOnTriggered.cs
@@ -5,7 +5,13 @@ namespace Sandbox.Vlad.Scripts
 	public class DeathOnTriggered : MonoBehaviour
 	{
 		[SerializeField] private string _deathText = "";
+		private AudioSource _deathRattle;
 		private PlayerDeathBehaviour _playerDeathBehaviour;
+
+		private void Start()
+		{
+			_deathRattle = GetComponent<AudioSource>();
+		}
 
 		public void OnTriggerEnter(Collider collider)
 		{
@@ -13,6 +19,7 @@ namespace Sandbox.Vlad.Scripts
 			if (_playerDeathBehaviour != null)
 			{
 				_playerDeathBehaviour.ChangeTextOfDeathCause(_deathText);
+				_deathRattle.Play();
 				StartCoroutine(_playerDeathBehaviour.ProcessOfDeath());
 			}
 		}
