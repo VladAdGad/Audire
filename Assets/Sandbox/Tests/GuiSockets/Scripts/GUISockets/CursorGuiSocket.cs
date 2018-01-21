811267cc815c2b559cf253938404603d41a640ac
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-21 05:28:49 +0100

    48 added cursor gui socket

---
+++ Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/CursorGuiSocket.cs
@@ -0,0 +1,19 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorGuiSocket : MonoBehaviour, IGUISocket
+{
+    [SerializeField] private Image _cursorImage;
+
+    //TODO add lock cursor on/off
+    public void Activate()
+    {
+        _cursorImage.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        _cursorImage.enabled = false;
+    }
+}
