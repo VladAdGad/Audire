641d02f11db0636b3245f323e6bedf2f4fd8c7d6
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-21 05:29:20 +0100

    48 refactored tooltip socket

--- Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/TooltipGuiSocket.cs
+++ Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/TooltipGuiSocket.cs
@@ -8,23 +8,9 @@ public class TooltipGuiSocket : MonoBehaviour, IGUISocket
     [SerializeField]
     private Text socketText;
 
-    public void Display(String text)
-    {
-        socketText.text = text;
-    }
+    public void Display(String text) => socketText.text = text;
+    public void Flush() => socketText.text = "";
     
-    public void Flush()
-    {
-        socketText.text = "";
-    }
-    
-    public void Activate()
-    {
-        enabled = true;
-    } 
-    
-    public void Deactivate()
-    {
-        enabled = false;
-    }
+    public void Activate() => socketText.enabled = true;
+    public void Deactivate() => socketText.enabled = false;
 }
