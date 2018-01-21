79483a84135609fdbaabbb3b2268492ad4f8111a
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-21 05:22:54 +0100

    48 added tooltip socket script

---
+++ Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/TooltipGuiSocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipGuiSocket : MonoBehaviour, IGUISocket
+{
+    [SerializeField]
+    private Text socketText;
+
+    public void Display(String text)
+    {
+        socketText.text = text;
+    }
+    
+    public void Flush()
+    {
+        socketText.text = "";
+    }
+    
+    public void Activate()
+    {
+        enabled = true;
+    } 
+    
+    public void Deactivate()
+    {
+        enabled = false;
+    }
+}
