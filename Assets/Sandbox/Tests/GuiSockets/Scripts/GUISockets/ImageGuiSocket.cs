35ca9db6da0d71ef81c73a0eb17ce2d0efbc7d96
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-21 15:39:18 +0100

    48 changed behaviour of image

--- Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/ImageGuiSocket.cs
+++ Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/ImageGuiSocket.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 
 public class ImageGuiSocket : MonoBehaviour, IGUISocket
 {
-    [Header("Image")]
-    [SerializeField] private RectTransform _imageRectTransform;
+    [Header("Image")] [SerializeField] private RectTransform _imageRectTransform;
     [SerializeField] private Image _image;
 
     private void Start() => _imageRectTransform.SetAsFirstSibling();
@@ -13,10 +12,13 @@ public class ImageGuiSocket : MonoBehaviour, IGUISocket
     public void Display(Sprite sprite)
     {
         _image.sprite = sprite;
-        _imageRectTransform.sizeDelta = sprite.rect.size;
+        _image.enabled = true;
     }
 
-    public void Flush() => _imageRectTransform.sizeDelta = Vector2.zero;
+    public void Flush()
+    {
+        _image.enabled = false;
+    }
 
     public void Activate() => enabled = true;
     public void Deactivate() => enabled = false;
