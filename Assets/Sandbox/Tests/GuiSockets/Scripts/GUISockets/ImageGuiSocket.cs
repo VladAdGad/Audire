e6e71140dcdc75707aa1370ceb6f2e0c52582fce
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-21 05:28:15 +0100

    48 added image gui socket

---
+++ Assets/Sandbox/Tests/GuiSockets/Scripts/GUISockets/ImageGuiSocket.cs
@@ -0,0 +1,22 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageGuiSocket : MonoBehaviour, IGUISocket
+{
+    [SerializeField]
+    private RectTransform _imageRectTransform;
+    [SerializeField]
+    private Image _image;
+    
+    public void Display(Sprite sprite)
+    {
+        _imageRectTransform.sizeDelta = sprite.rect.size;
+        _image.sprite = sprite;
+    }
+    
+    public void Flush() => _imageRectTransform.sizeDelta = Vector2.zero;
+    
+    public void Activate() => enabled = true;
+    public void Deactivate() => enabled = false;
+}
