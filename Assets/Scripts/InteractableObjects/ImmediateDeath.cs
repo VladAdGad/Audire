a3abc7c6f810b57499b3c8606bfb45129c13f2bb
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-22 18:57:27 +0100

    implementing new deathCase of death

---
+++ Assets/Scripts/InteractableObjects/ImmediateDeath.cs
@@ -0,0 +1,29 @@
+﻿using Player;
+using UnityEngine;
+
+namespace InteractableObjects
+{
+    public class ImmediateDeath : MonoBehaviour
+    {
+        [SerializeField] private string _deathText = "";
+        [SerializeField] [Range(0, 10)] private float _secondsBeforeDeath = .0f;
+        [SerializeField] private PlayerDeathBehaviour _playerDeathBehaviour;
+        [SerializeField] private DoorBehaviour _lockDoorBehaviour;
+        [SerializeField] private GameObject _sprite;
+
+        private void StartEventImmediateDeath()
+        {
+            if (_lockDoorBehaviour.GetStateOfDoor())
+            {
+                _sprite.SetActive(true);
+                _playerDeathBehaviour.StartProcessOfDeath(_secondsBeforeDeath, _deathText);
+                DestroyGameObject();
+            }
+        }
+        
+        private void DestroyGameObject()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
