44801804acd07be385175ca8d4147c951d7e5150
4th identity of Vlad U <13950@pjwstk.edu.pl>
2018-01-24 15:07:24 +0100

    171 added door and lock scripts

---
+++ Assets/Scripts/InteractableObjects/Doors/DoorKey.cs
@@ -0,0 +1,21 @@
+﻿using EventManagement;
+using UnityEngine;
+
+namespace InteractableObjects.Doors
+{
+    public class DoorKey : MonoBehaviour, IPressable
+    {
+        [SerializeField] private KeyLockedDoor _doorToOpen;
+        [SerializeField] private KeyCode _activationButton = KeyCode.E;
+        [SerializeField] private AudioSource _soundOnPickUpAudioSource;
+
+        public KeyCode ActivationKeyCode() => _activationButton;
+
+        public void OnPress()
+        {
+            _doorToOpen.UnlockDoor();
+            _soundOnPickUpAudioSource.Play();
+            gameObject.active = false;
+        }
+    }
+}
---
+++ Assets/Scripts/InteractableObjects/Doors/KeyLockedDoor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using EventManagement;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace InteractableObjects.Doors
+{
+    public class KeyLockedDoor : MonoBehaviour, IGazable, IPressable
+    {
+        [SerializeField] private TooltipGuiSocket _tooltipGuiSocket;
+        [SerializeField] private String _toolTipText;
+        [SerializeField] private String _lockedStateTooltip;
+        [SerializeField] private KeyCode _activationButton = KeyCode.E;
+
+        [SerializeField] private AudioSource _openingDoorAudioSource;
+        [SerializeField] private AudioSource _closingDoorAudioSource;
+
+        private bool _stateOfDoor = false;
+        private Animator _animator;
+
+        private bool _doorLocked = true;
+        private bool _doorClosed = true;
+
+        public void UnlockDoor() => _doorLocked = false;
+
+
+        public void OnGazeEnter() => _tooltipGuiSocket.Display(_doorLocked ? _lockedStateTooltip : _toolTipText);
+        public void OnGazeExit() => _tooltipGuiSocket.Flush();
+
+
+        public KeyCode ActivationKeyCode() => _activationButton;
+
+        public void OnPress()
+        {
+            if (_doorLocked) return;
+
+            if (_doorClosed)
+                OpenDoor();
+            else
+                CloseDoor();
+        }
+
+        private void OpenDoor()
+        {
+            if (isDoorInMotion()) return;
+            
+            _animator.SetBool("open", true);
+            _openingDoorAudioSource.Play();
+
+            _doorClosed = false;
+        }
+
+        private void CloseDoor()
+        {
+            if (isDoorInMotion()) return;
+
+            _closingDoorAudioSource.Play();
+            _animator.SetBool("open", false);
+
+            _doorClosed = true;
+        }
+
+        //TODO @Vlad change this costyl na triggery po okonchaniju animacji, sozdaj boolean id door closed i mienaj s
+        //pomoshciu metod etot boolin, a w etoj wozwrashchaj sostojanije
+        private bool isDoorInMotion() => _openingDoorAudioSource.isPlaying || _closingDoorAudioSource.isPlaying;
+
+
+        private void Start() => _animator = transform.parent.parent.GetComponent<Animator>();
+
+        private void OnValidate() =>
+            Assert.AreNotEqual(_activationButton, KeyCode.None, "Door Actiation button must not be null.");
+    }
+}
