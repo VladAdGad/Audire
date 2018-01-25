78a95a54398284f2e3d76a1d474a891a58ed5fff
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-25 08:33:52 +0100

    181 adjusted lamp script to new tooltip

--- Assets/Scripts/InteractableObjects/LampBehaviour.cs
+++ Assets/Scripts/InteractableObjects/LampBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using EventManagement;
 using EventManagement.Interfaces;
+using Gui;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -7,7 +8,9 @@ namespace InteractableObjects
 {
     public class LampBehaviour : MonoBehaviour, IPressable, IGazable
     {
-        [SerializeField] private GUISkin _skin;
+        [SerializeField] private TooltipGuiSocket _tooltipGuiSocket;
+        [SerializeField] private string _toolTipOn;
+        [SerializeField] private string _toolTipOff;
         [SerializeField] private KeyCode _activationButton = KeyCode.E;
         [SerializeField] private GameObject _lights;
         private AudioSource _turningLapmSound;
@@ -18,24 +21,13 @@ namespace InteractableObjects
             _turningLapmSound = GetComponent<AudioSource>();
         }
 
-        private void OnGUI()
-        {
-            if (_isLookingLamp)
-            {
-                GUI.skin = _skin;
-                GUI.TextArea(TipToInteractReactangle(), "TO TURN ON/OFF PRESS " + _activationButton);
-            }
-        }
+        public void OnGazeEnter() => _tooltipGuiSocket.Display(!_lights.activeSelf
+            ? $"{_toolTipOn} {_activationButton}"
+            : $"{_toolTipOff} {_activationButton}");
 
-        private static Rect TipToInteractReactangle() => new Rect(
-            Screen.width / 2 - Screen.width / 6,
-            Screen.height / 2 + Screen.height / 4,
-            Screen.width / 3f,
-            Screen.width / 2 - 2 * Screen.width / 5);
+        public void OnGazeExit() => _tooltipGuiSocket.Flush();
 
         public KeyCode ActivationKeyCode() => _activationButton;
-
-        private void PlaySound() => _turningLapmSound.Play();
 
         public void OnPress()
         {
@@ -43,12 +35,10 @@ namespace InteractableObjects
             PlaySound();
         }
 
-        public void OnGazeEnter() => _isLookingLamp = true;
-        public void OnGazeExit() => _isLookingLamp = false;
+        private void PlaySound() => _turningLapmSound.Play();
 
         private void OnValidate()
         {
-            Assert.IsNotNull(_skin);
             Assert.IsTrue(_activationButton != KeyCode.None);
         }
     }
