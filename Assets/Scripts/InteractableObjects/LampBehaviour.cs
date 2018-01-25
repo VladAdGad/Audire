6ef58d36cba8857fed27bdd0a3f989c8ba856f5c
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-25 09:07:42 +0100

    181 fixed bug with no refresh tooltip when you look at lamp and it does not change

--- Assets/Scripts/InteractableObjects/LampBehaviour.cs
+++ Assets/Scripts/InteractableObjects/LampBehaviour.cs
@@ -13,11 +13,12 @@ namespace InteractableObjects
         [SerializeField] private KeyCode _activationButton = KeyCode.E;
         [SerializeField] private GameObject _lights;
         private AudioSource _turningLapmSound;
-        private bool _isLookingLamp = false;
+        private bool _isLampOn = false;
 
         private void Start()
         {
             _turningLapmSound = GetComponent<AudioSource>();
+            _isLampOn = _lights.activeSelf;
         }
 
         public void OnGazeEnter() => _tooltipGuiSocket.Display(!_lights.activeSelf
@@ -30,9 +31,28 @@ namespace InteractableObjects
 
         public void OnPress()
         {
-            _lights.SetActive(!_lights.activeSelf);
+            if (_isLampOn)
+                LampOff();
+            else
+                LampOn();
+
+            ChangeLampState();
             PlaySound();
         }
+
+        private void LampOn()
+        {
+            _tooltipGuiSocket.Display($"{_toolTipOff} {_activationButton}");
+            _lights.SetActive(true);
+        }
+
+        private void LampOff()
+        {
+            _tooltipGuiSocket.Display($"{_toolTipOn} {_activationButton}");
+            _lights.SetActive(false);
+        }
+
+        private void ChangeLampState() => _isLampOn = !_isLampOn;
 
         private void PlaySound() => _turningLapmSound.Play();
 
