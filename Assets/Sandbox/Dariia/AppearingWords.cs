07e9638c8056663aed77ae7c93ba0a9771f8f591
DashaShev <sdasha321@gmail.com>
2018-01-28 23:41:55 +0100

    created appearing words in lvl1, added new font and script

---
+++ Assets/Sandbox/Dariia/AppearingWords.cs
@@ -0,0 +1,26 @@
+﻿using Triggers;
+using UnityEngine;
+
+public class AppearingWords : MonoBehaviour {
+
+    [SerializeField] private APassiveTriggerable _triggerOnSuccess1;
+    [SerializeField] private APassiveTriggerable _triggerOnSuccess2;
+    [SerializeField] private APassiveTriggerable _triggerOnSuccess3;
+    [SerializeField] private APassiveTriggerable _triggerOnSuccess4;
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _triggerOnSuccess1.OnTrigger();
+            _triggerOnSuccess2.OnTrigger();
+            _triggerOnSuccess3.OnTrigger();
+            _triggerOnSuccess4.OnTrigger();
+        }
+    }
+}
