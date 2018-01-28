8b16562476bcc66d768235eeafa9d534cdfd7581
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-28 06:37:42 +0100

    added button for hyperlinks

---
+++ Assets/Scripts/Gui/ButtonHyperLink.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gui
+{
+    public class ButtonHyperLink : MonoBehaviour
+    {
+        [SerializeField] private string _hyperLink;
+
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(() => Application.OpenURL(_hyperLink));
+        }
+    }
+}
