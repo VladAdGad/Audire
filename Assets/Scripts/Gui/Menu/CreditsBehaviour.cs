aa69ac526ae5ff2feb83d117545e1900579ed2be
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-28 03:06:41 +0100

    143 after credits end - load level0

---
+++ Assets/Scripts/Gui/Menu/CreditsBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Gui.Menu
+{
+    public class CreditsBehaviour : MonoBehaviour
+    {
+        private const int MainMenuIndex = 0;
+
+        public void LoadMainLevelAfterCredits()
+        {
+            SceneManager.LoadScene(MainMenuIndex);
+        }
+    
+    }
+}
