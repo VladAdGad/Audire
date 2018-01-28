3ebefbaaf74431e53807efef339a1af88cfd93cc
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-28 06:39:40 +0100

    added validate

--- Assets/Scripts/Gui/ButtonHyperLink.cs
+++ Assets/Scripts/Gui/ButtonHyperLink.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 namespace Gui
@@ -10,6 +11,11 @@ namespace Gui
         private void Awake()
         {
             GetComponent<Button>().onClick.AddListener(() => Application.OpenURL(_hyperLink));
+        }
+
+        private void OnValidate()
+        {
+            Assert.IsNotNull(_hyperLink);
         }
     }
 }
