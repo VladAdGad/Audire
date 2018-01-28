54ce0ee5d49a23066288a1a2712c025e98028f23
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-28 03:54:52 +0100

    143 clean code PauseBehaviour

--- Assets/Scripts/Gui/Menu/PauseBehaviour.cs
+++ Assets/Scripts/Gui/Menu/PauseBehaviour.cs
@@ -6,25 +6,24 @@ namespace Gui.Menu
 {
     public class PauseBehaviour : MonoBehaviour
     {
-        [SerializeField] private Transform _pauseMenuCanvas;
         [SerializeField] private AudioMixerSnapshot _unpaused;
         [SerializeField] private AudioMixerSnapshot _paused;
 
         public void PauseOrUnpause()
         {            
-            if (!_pauseMenuCanvas.gameObject.activeSelf)
+            if (gameObject.activeSelf)
             {
-                _pauseMenuCanvas.gameObject.SetActive(!_pauseMenuCanvas.gameObject.activeSelf);
-                PlayerBehaviour.PlayerOnPause(false);
-                Time.timeScale = 0;
-                _paused.TransitionTo(.01f);
-            }
-            else
-            {
-                _pauseMenuCanvas.gameObject.SetActive(!_pauseMenuCanvas.gameObject.activeSelf);
+                gameObject.SetActive(false);
                 PlayerBehaviour.PlayerOnPause(true);
                 Time.timeScale = 1;
                 _unpaused.TransitionTo(.01f);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+                PlayerBehaviour.PlayerOnPause(false);
+                Time.timeScale = 0;
+                _paused.TransitionTo(.01f);
             }
         }
     }
