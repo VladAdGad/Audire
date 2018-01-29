e84793693a55aa175bf9c2795bc3430bc4eb1de7
4th identity of Vlad U <13950@pjwstk.edu.pl>
2018-01-29 04:49:24 +0100

    25 added clamp script

---
+++ Assets/Sandbox/Tests/Features/DoorObsticle/Scripts/ObsticleClamp.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Sandbox.Tests.Features.LockDrum.Scripts.Unlockables;
+using UnityEngine;
+
+namespace Assets.Sandbox.Tests.Features.DoorObsticle.Scripts
+{
+    public class ObsticleClamp : AUnlockable
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _unlockAnimationName;
+        [SerializeField] private DoorObsticleBehaviuor _doorObsticle;
+        public override void Unlock() => _animator.Play(_unlockAnimationName);
+
+        public void UnlockObsticle() => _doorObsticle.UnlockObsticle();
+    }
+}
