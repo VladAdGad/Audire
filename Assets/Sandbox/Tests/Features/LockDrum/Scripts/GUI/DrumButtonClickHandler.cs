4ff536ab317e9f5cebd79fd6a7bea521f2e61926
4th identity of Vlad U <13950@pjwstk.edu.pl>
2018-01-29 03:32:21 +0100

    223 drum lock gui scripts refactor

---
+++ Assets/Sandbox/Tests/Features/LockDrum/Scripts/GUI/DrumButtonClickHandler.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Sandbox.Tests.Features.LockDrum.Scripts.Lock;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Sandbox.Tests.Features.LockDrum.Scripts.GUI
+{
+    public class DrumButtonClickHandler : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private GameObject _pauseSocket;
+        private DrumLock _drumLock;
+        private int _drumIndex;
+        private Text _buttonText;
+
+        private void Awake() => _buttonText = GetComponentInChildren<Text>();
+        public void SetButtonIndex(int index) => _drumIndex = index;
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (IsGamePaused()) return;
+            ChangeDrumNumber(eventData);
+        }
+        private bool IsGamePaused() => _pauseSocket.activeSelf;
+        
+        private void ChangeDrumNumber(PointerEventData eventData)
+        {
+            string newDrumNumber = NumberRollingUtility.RollNextNumber(eventData.button, _buttonText.text);
+            _buttonText.text = newDrumNumber;
+            _drumLock.DrumChanged(_drumIndex, newDrumNumber);
+        }
+
+        public void ActivateButtonWith(DrumLock drumLock)
+        {
+            gameObject.SetActive(true);
+            _drumLock = drumLock;
+            _buttonText.text = drumLock.GetCurrentCodeAt(_drumIndex);
+        }
+        
+        public void DeactivateButton()
+        {
+            gameObject.SetActive(false);
+            _drumLock = null;
+        }
+    }
+}
---
+++ Assets/Sandbox/Tests/Features/LockDrum/Scripts/GUI/DrumLockGuiSocket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Sandbox.Tests.Features.LockDrum.Scripts.Lock;
+using Gui.Interfaces;
+using UnityEngine;
+
+namespace Assets.Sandbox.Tests.Features.LockDrum.Scripts.GUI
+{
+    public class DrumLockGuiSocket : MonoBehaviour, IGuiSocket
+    {
+        [SerializeField] private List<DrumButtonClickHandler> _buttons;
+
+        public void Display(DrumLock drumLock) => _buttons.ForEach(button => button.ActivateButtonWith(drumLock));
+        public void Flush() => _buttons.ForEach(button => button.DeactivateButton());
+
+        public void Activate() => gameObject.SetActive(true);
+        public void Deactivate() => gameObject.SetActive(false);
+
+        private void Start() => ApplyButtonIndexNumbers();
+        private void ApplyButtonIndexNumbers() =>
+            Enumerable
+                .Range(0, _buttons.Count)
+                .ToList()
+                .ForEach(index => _buttons[index].SetButtonIndex(index));
+    }
+}
