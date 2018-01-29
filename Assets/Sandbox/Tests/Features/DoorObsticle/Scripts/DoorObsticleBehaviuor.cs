445d4b7b77137d99400b3f396cd05e97e17902c6
4th identity of Vlad U <13950@pjwstk.edu.pl>
2018-01-29 04:51:39 +0100

    225 updated scene with feature objects

--- Assets/Sandbox/Tests/Features/DoorObsticle/Scripts/DoorObsticleBehaviuor.cs
+++ Assets/Sandbox/Tests/Features/DoorObsticle/Scripts/DoorObsticleBehaviuor.cs
@@ -1,15 +1,21 @@
 ﻿using EventManagement.Interfaces;
+using Gui;
 using InteractableObjects.Doors;
 using UnityEngine;
 
 namespace Assets.Sandbox.Tests.Features.DoorObsticle.Scripts
 {
     [RequireComponent(typeof(Animator))]
-    public class DoorObsticleBehaviuor : MonoBehaviour, IPressable
+    public class DoorObsticleBehaviuor : MonoBehaviour, IPressable, IGazable
     {
+        [SerializeField] private TooltipGuiSocket _tooltipGuiSocket;
+        [SerializeField] private string _onClosedTooltip;
+        [SerializeField] private string _onOpenTooltip;
+        [SerializeField] private string _whenDoorIsOpenTooltip;
+        
         [SerializeField] private KeyCode activationKeyCode = KeyCode.E;
 
-        [SerializeField] private DoorBehaviour _doorBehaviour;
+        [SerializeField] private KeyLockedDoor _lockedDoor;
         [SerializeField] private bool _closed = true;
         [SerializeField]  private bool _blockedByLock;
 
@@ -27,9 +33,12 @@ namespace Assets.Sandbox.Tests.Features.DoorObsticle.Scripts
         public KeyCode ActivationKeyCode() => activationKeyCode;
         public void OnPress() => ChangeMovingState();
 
+        public void OnGazeEnter() => _tooltipGuiSocket.Display(_blockedByLock ? _onClosedTooltip : (_lockedDoor.IdDoorOpen() ? _whenDoorIsOpenTooltip : _onOpenTooltip));
+        public void OnGazeExit() => _tooltipGuiSocket.Flush();
+        
         private void ChangeMovingState()
         {
-            if (_animationInProgress || _blockedByLock) return;
+            if (_animationInProgress || _blockedByLock || _lockedDoor.IdDoorOpen()) return;
         
             if (_closed)
                 StartOpeningRotation();
@@ -61,8 +70,8 @@ namespace Assets.Sandbox.Tests.Features.DoorObsticle.Scripts
             SetAnimationInProgress(false);
         }
 
-        private void BlockDoor() => _doorBehaviour.gameObject.active = false;
-        private void UnBlockDoor() => _doorBehaviour.gameObject.active = true;
+        private void BlockDoor() => _lockedDoor.LockDoor();
+        private void UnBlockDoor() => _lockedDoor.UnlockDoor();
 
         public void UnlockObsticle()
         {
