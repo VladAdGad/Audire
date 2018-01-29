e2e2ba75e165f87608c741ef3441c9affef8b48d
4th identity of Vlad U <13950@pjwstk.edu.pl>
2018-01-29 03:33:15 +0100

    223 added AnimationOnTrigger  and unlockable base

---
+++ Assets/Sandbox/Tests/Features/LockDrum/Scripts/Triggerables/AnimateOnTrigger.cs
@@ -0,0 +1,12 @@
+﻿using Triggers;
+using UnityEngine;
+
+namespace Assets.Sandbox.Tests.Features.LockDrum.Scripts.Triggerables
+{
+    public class AnimateOnTrigger : APassiveTriggerable
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _animationStateName;
+        public override void OnTrigger() => _animator.Play(_animationStateName);
+    }
+}
---
+++ Assets/Sandbox/Tests/Features/LockDrum/Scripts/Unlockables/AUnlockable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Sandbox.Tests.Features.LockDrum.Scripts.Unlockables
+{
+    public abstract class AUnlockable : MonoBehaviour
+    {
+        public abstract void Unlock();
+    }
+}
