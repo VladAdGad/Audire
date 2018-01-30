6e6ae25cc70c6425d9ac8d3900606a845989a74f
DESKTOP-P9H5HAN\VladAdGad <destinylineagukvladgad@gmail.com>
2018-01-30 02:46:52 +0100

    249 reimplementig trigger for DoorKey - separate trigger and main responsibility

---
+++ Assets/Scripts/Triggers/TriggerableImplementations/DoorKeyTriggerPianoPlaying.cs
@@ -0,0 +1,16 @@
+﻿using InteractableObjects.Doors;
+using UnityEngine;
+
+namespace Triggers.TriggerableImplementations
+{
+    public class DoorKeyTriggerPianoPlaying : DoorKey
+    {
+        [SerializeField] private APassiveTriggerable _triggerPianoPlaying;
+        
+        public override void OnPress()
+        {
+            base.OnPress();
+            _triggerPianoPlaying.OnTrigger();
+        }
+    }
+}
