dbf9f135ca06262ec0c0ae552f7d1e209ef4865c
Konrad <admin@elenx.net>
2018-02-05 17:13:54 +0100

    Extracting lamp tooltip and fixing typo.

--- Assets/Scripts/InteractableObjects/LampBehaviour.cs
+++ Assets/Scripts/InteractableObjects/LampBehaviour.cs
@@ -13,18 +13,18 @@ namespace InteractableObjects
         [SerializeField] private KeyCode _activationButton = KeyCode.E;
         [SerializeField] private GameObject _lights;
         
-        private AudioSource _turningLapmSound;
-        private bool _isLampOn = false;
+        private AudioSource _turningLampSound;
+        private bool _isLampOn;
 
         private void Start()
         {
-            _turningLapmSound = GetComponent<AudioSource>();
+            _turningLampSound = GetComponent<AudioSource>();
             _isLampOn = _lights.activeSelf;
         }
 
-        public void OnGazeEnter() => _tooltipGuiSocket.Display(!_lights.activeSelf
-            ? $"{_toolTipOn} {_activationButton}"
-            : $"{_toolTipOff} {_activationButton}");
+        public void OnGazeEnter() => _tooltipGuiSocket.Display(LampTooltip());
+
+        private string LampTooltip() => _lights.activeSelf ? $"{_toolTipOff} {_activationButton}" : $"{_toolTipOn} {_activationButton}";
 
         public void OnGazeExit() => _tooltipGuiSocket.Flush();
 
@@ -55,7 +55,7 @@ namespace InteractableObjects
 
         private void ChangeLampState() => _isLampOn = !_isLampOn;
 
-        private void PlaySound() => _turningLapmSound.Play();
+        private void PlaySound() => _turningLampSound.Play();
 
         private void OnValidate() => Assert.IsTrue(_activationButton != KeyCode.None);
     }
